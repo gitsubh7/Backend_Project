ec569181a16495ef304f985a147b17dbab9e6860
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-06-13 00:21:23 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ dotenv.config({
     path: "./env"
 });
 
+
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants.js"
 import express from "express";
 import connectDB from "./db/index.js";
 connectDB().then(()=>{  
     const app = express();
-    app.listen(process.env.PORT || 3000, ()=>{console.log(`Server is running on port ${process.env.PORT}`);})
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+        // console.log(`running on url http://localhost:${process.env.PORT}`);
+    })
+
 }).catch((error)=>{console.log("Error connecting to MongoDB", error);}) 
 
 
