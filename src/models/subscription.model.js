0e4ab234f10cc031b6140a48fd440c7cf0d3cc1b
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-06-17 14:29:27 +0300

    subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,13 @@
+import mongoose,{Schema} from "mongoose";
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,//one who is subscribgin
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,//one to whom the subscriber is subscribing
+        ref:"User"
+    }
+},{timestamps:true})
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema);
