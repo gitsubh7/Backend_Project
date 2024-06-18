a9d0e567351f3843535dfdf8c3c3aa26feaa07d6
Abdul Subhan <abduls.ug22.cs@nitp.ac.in>
2024-06-18 23:57:19 +0300

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, registerUser, updateAccountDetails, updateUserAvatar } from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js'
 import { loginUser } from '../controllers/user.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { logOutUser } from '../controllers/user.controller.js'; 
 import { refreshAccessToken } from '../controllers/user.controller.js';
+import { get } from 'mongoose';
 const router = Router()
 router.route("/register").post(
     upload.fields([
@@ -21,4 +22,14 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-export default router 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserAvatar)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
+
+export default router
